132bc253d0e67b3630bb1f180e9368b1099a76f7
Pie-Jie <pie.jie@hotmail.com>
2017-11-10 01:03:30 +0100

    Updated index.html & index.js for optimalization

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 var Twitter = require('twitter');
 var dataArr = [];
+var tweet;
 
 
 
@@ -12,22 +13,15 @@ var T = new Twit({
   consumer_secret:      'cmCfoDrOp67u7a8bsY0sB6fk0Aayhql41ZrVNFBEtqgK4FYEHx',
   access_token:         '42051552-prICERMck8rcpPKYG38fcSHggHuQ2vijI8Ro4mmDf',
   access_token_secret:  'mQmlRdOAirzXiBG2SHf8hwUSA25t2KHBVUN58K1Klongm',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  timeout_ms:           60*1000,  
 });
 
-//
-//  filter the twitter public stream by the word 'mango'.
-//
 var stream = T.stream('user', { track: 'trump', user_name: 'PieJieD' });
 
 stream.on('message', function (msg) {
-    //console.log(msg.text);
     tweet = msg.text;
     dataArr.push(tweet);
 });
-var data2send = {
-    Mr_Robot: "hello_friend"
-};
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -35,7 +29,6 @@ app.get('/', function (req, res) {
 
 app.get('/getdata', function (req, res) {
     res.send(dataArr);
-    console.log('getting data');
 });
 
 var port = process.env.PORT || 3000;
