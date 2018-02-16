a84032dfc1235d455c63c7e1379e3b3896c8d926
Pieter-Jan Desmadryl <pj.desmadryl@gmail.com>
2018-02-16 15:11:48 +0100

    Update index.js
    Removed twitter security data

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ var tweet;
  var Twit = require('twit');
 
 var T = new Twit({
-  consumer_key:         'Zh89x9BRtSBeBwYrvHtsNxdEv',
-  consumer_secret:      'cmCfoDrOp67u7a8bsY0sB6fk0Aayhql41ZrVNFBEtqgK4FYEHx',
-  access_token:         '42051552-prICERMck8rcpPKYG38fcSHggHuQ2vijI8Ro4mmDf',
-  access_token_secret:  'mQmlRdOAirzXiBG2SHf8hwUSA25t2KHBVUN58K1Klongm',
-  timeout_ms:           60*1000,  
+    consumer_key: 'INSERT CONSUMER KEY HERE',
+    consumer_secret: 'INSERT CONSUMER SECRET HERE',
+    access_token: 'INSERT ACCESS TOKEN HERE',
+    access_token_secret: 'INSERT ACCESS TOKEN SECRET HERE',
+    timeout_ms: 60 * 1000,
 });
-
 var stream = T.stream('user', { track: 'trump', user_name: 'PieJieD' });
 
 stream.on('message', function (msg) {
